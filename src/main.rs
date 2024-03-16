b22b12d5a4d063ab251e03969b10d7dcff08d78b
thrombe <yash.n.thombare620@gmail.com>
2024-03-17 00:23:42 +0530

    added cli + switch-to-workspace command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 use clap::{arg, command, Parser, Subcommand};
 use hyprland::{
     data::{CursorPosition, Monitor, Workspace},
@@ -19,7 +20,7 @@ struct Cli {
 
 #[derive(Subcommand, Debug)]
 pub enum Command {
-    InitLoop,
+    MouseLoop,
     MoveRight,
     MoveLeft,
     MoveUp,
@@ -31,83 +32,136 @@ pub enum Command {
         #[arg(short, long)]
         name: String,
     },
+    SwitchToWorkspace {
+        /// workspace name
+        #[arg(short, long)]
+        name: String,
+    },
+}
+
+#[derive(Debug)]
+pub struct State {
+    pub activities: Vec<String>,
+    pub workspaces: Vec<Vec<String>>,
+}
+
+impl State {
+    fn new() -> Self {
+        let raw_workspaces = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+        let activities = ["issac", "colg"]
+            .into_iter()
+            .map(String::from)
+            .collect::<Vec<_>>();
+        let cooked_workspaces = activities
+            .iter()
+            .map(|name| {
+                raw_workspaces
+                    .iter()
+                    .cloned()
+                    .map(|id| format!("{name}:{id}"))
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
+
+        Self {
+            activities,
+            workspaces: cooked_workspaces,
+        }
+    }
+
+    fn get_activity_index(&self, name: impl AsRef<str>) -> Option<usize> {
+        let name = name.as_ref();
+        let activity_index = self.activities.iter().position(|a| name.starts_with(a))?;
+        Some(activity_index)
+    }
+
+    /// (activity index, workspace index)
+    fn get_indices(&self, name: impl AsRef<str>) -> Option<(usize, Option<usize>)> {
+        let name = name.as_ref();
+        let activity_index = self.get_activity_index(name)?;
+        let workspace_index = self.workspaces[activity_index]
+            .iter()
+            .position(|w| w == name);
+        Some((activity_index, workspace_index))
+    }
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let raw_workspaces = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-    let activities = ["issac", "colg"];
-    let cooked_workspaces = activities
-        .iter()
-        .map(|name| {
-            raw_workspaces
-                .iter()
-                .cloned()
-                .map(|id| format!("{name}:{id}"))
-                .collect::<Vec<_>>()
-        })
-        .collect::<Vec<_>>();
+    let cli = Cli::parse();
+    let state = State::new();
 
-    Dispatch::call_async(DispatchType::Workspace(
-        WorkspaceIdentifierWithSpecial::Name(&cooked_workspaces[0][0]),
-    ))
-    .await?;
+    match cli.command {
+        Command::MouseLoop => {
+            Dispatch::call_async(DispatchType::Workspace(
+                WorkspaceIdentifierWithSpecial::Name(&state.workspaces[0][0]),
+            ))
+            .await?;
 
-    // TODO: multi monitor setup yaaaaaaaaaaaaaaaaa
-    let monitor = Monitor::get_active_async().await?;
+            // TODO: multi monitor setup yaaaaaaaaaaaaaaaaa
+            let monitor = Monitor::get_active_async().await?;
 
-    loop {
-        tokio::time::sleep(std::time::Duration::from_millis(300)).await;
-        let mut c = CursorPosition::get_async().await?;
-        let mut y = 0;
-        let mut x = 0;
-        if c.x == 0 {
-            x += 3 - 1;
-            c.x = monitor.width as i64 - 2;
-        } else if c.x == monitor.width as i64 - 1 {
-            x += 1;
-            c.x = 2;
+            loop {
+                tokio::time::sleep(std::time::Duration::from_millis(300)).await;
+                let mut c = CursorPosition::get_async().await?;
+                let mut y = 0;
+                let mut x = 0;
+                if c.x == 0 {
+                    x += 3 - 1;
+                    c.x = monitor.width as i64 - 2;
+                } else if c.x == monitor.width as i64 - 1 {
+                    x += 1;
+                    c.x = 2;
+                }
+                if c.y == 0 {
+                    y += 3 - 1;
+                    c.y = monitor.height as i64 - 2;
+                } else if c.y == monitor.height as i64 - 1 {
+                    y += 1;
+                    c.y = 2;
+                }
+
+                if x + y == 0 {
+                    continue;
+                }
+
+                let workspace = Workspace::get_active_async().await?;
+                let Some((current_activity_index, Some(current_workspace_index))) =
+                    state.get_indices(&workspace.name)
+                else {
+                    println!("unknown workspace {}", workspace.name);
+                    continue;
+                };
+
+                y += current_workspace_index / 3;
+                y %= 3;
+                x += current_workspace_index % 3;
+                x %= 3;
+
+                let new_workspace = &state.workspaces[current_activity_index][y * 3 + x];
+                if new_workspace != &workspace.name {
+                    Dispatch::call_async(DispatchType::Workspace(
+                        WorkspaceIdentifierWithSpecial::Name(new_workspace),
+                    ))
+                    .await?;
+                    Dispatch::call_async(DispatchType::MoveCursor(c.x, c.y)).await?;
+                }
+            }
         }
-        if c.y == 0 {
-            y += 3 - 1;
-            c.y = monitor.height as i64 - 2;
-        } else if c.y == monitor.height as i64 - 1 {
-            y += 1;
-            c.y = 2;
-        }
-
-        if x + y == 0 {
-            continue;
-        }
-
-        let workspace = Workspace::get_active_async().await?;
-        let Some(current_activity_index) = activities
-            .iter()
-            .position(|a| workspace.name.starts_with(a))
-        else {
-            println!("unknown workspace {}", workspace.name);
-            continue;
-        };
-        let Some(current_workspace_index) = cooked_workspaces[current_activity_index]
-            .iter()
-            .position(|w| w == &workspace.name)
-        else {
-            println!("unknown workspace {}", workspace.name);
-            continue;
-        };
-
-        y += current_workspace_index / 3;
-        y %= 3;
-        x += current_workspace_index % 3;
-        x %= 3;
-
-        let new_workspace = &cooked_workspaces[current_activity_index][y * 3 + x];
-        if new_workspace != &workspace.name {
+        Command::SwitchToWorkspace { name } => {
+            let (activity_index, workspace_index) =
+                state.get_indices(&name).context("activity not found")?;
+            let workspace_index = workspace_index.context("workspace not found")?;
+            let new_workspace = &state.workspaces[activity_index][workspace_index];
             Dispatch::call_async(DispatchType::Workspace(
                 WorkspaceIdentifierWithSpecial::Name(new_workspace),
             ))
             .await?;
-            Dispatch::call_async(DispatchType::MoveCursor(c.x, c.y)).await?;
+        }
+        _ => {
+            todo!()
         }
     }
+
+    Ok(())
 }
