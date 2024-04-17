630d4ec4ed845cfcd4721bdb6e7e5e501aadd815
thrombe <yash.n.thombare620@gmail.com>
2024-04-18 04:01:28 +0530

    plugin: set correct animation in hook

--- plugin/src/main.cpp
+++ plugin/src/main.cpp
@@ -1,22 +1,37 @@
 
 #include <cerrno>
 #include <cstdio>
+#include <exception>
+#include <filesystem>
 #include <hyprland/src/config/ConfigManager.hpp>
 #include <hyprland/src/debug/Log.hpp>
 #include <hyprland/src/desktop/Workspace.hpp>
 #include <hyprland/src/helpers/AnimatedVariable.hpp>
 #include <hyprland/src/plugins/PluginAPI.hpp>
+#include <netinet/in.h>
+#include <poll.h>
+#include <pthread.h>
 #include <stdexcept>
 #include <string>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <thread>
-#include <filesystem>
 
-namespace fs = std::filesystem;
+enum Animation {
+    None = 0,
+    Left = 1,
+    Right = 2,
+    Up = 3,
+    Down = 4,
+    Fade = 5,
+};
+Animation anim_dir = Animation::None;
 
 inline HANDLE PHANDLE = nullptr;
 std::string sock_path;
+bool exit_flag = false;
+int sockfd = -1;
+std::thread sock_thread;
 
 std::string get_socket_path() {
     const auto ISIG = getenv("HYPRLAND_INSTANCE_SIGNATURE");
@@ -25,8 +40,8 @@ std::string get_socket_path() {
         throw std::runtime_error("[hyprkool] could not get HYPRLAND_INSTANCE_SIGNATURE");
     }
     auto sock_path = "/tmp/hyprkool/" + std::string(ISIG);
-    if (!fs::exists(sock_path)) {
-        if (!fs::create_directories(sock_path)) {
+    if (!std::filesystem::exists(sock_path)) {
+        if (!std::filesystem::create_directories(sock_path)) {
             std::cout << "could not create directory";
             throw std::runtime_error("could not create directory");
         }
@@ -51,7 +66,13 @@ void socket_connect(int clientfd) {
         std::istringstream iss(partial_line + std::string(buffer, bytes_read));
         std::string line;
         while (std::getline(iss, line)) {
-            std::cout << "Received line: " << line << std::endl;
+            try {
+                Animation d = static_cast<Animation>(std::stoi(line));
+                anim_dir = d;
+            } catch (const std::exception& e) {
+                std::cerr << "Error parsing socket data: " << e.what() << std::endl;
+                continue;
+            }
         }
 
         partial_line = line;
@@ -59,10 +80,14 @@ void socket_connect(int clientfd) {
 }
 
 void socket_serve() {
-    int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
     if (sockfd < 0) {
         std::cerr << "Error creating socket" << std::endl;
         throw std::runtime_error("[hyprkool] Error creating socket");
+    }
+
+    if (std::filesystem::exists(sock_path)) {
+        auto _ = std::filesystem::remove(sock_path);
     }
 
     // Bind the socket to a file path (socket file)
@@ -75,7 +100,19 @@ void socket_serve() {
         throw std::runtime_error("[hyprkool] Error binding socket");
     }
 
-    while (true) {
+    pollfd fd;
+    fd.fd = sockfd;
+    fd.events = POLLIN;
+    while (!exit_flag) {
+        int ret = poll(&fd, 1, 50);
+        if (ret < 0) {
+            std::cerr << "Error polling on socket" << std::endl;
+            throw std::runtime_error("[hyprkool] Error polling on socket");
+        } else if (ret == 0) {
+            // timeout
+            continue;
+        }
+
         if (listen(sockfd, 5) < 0) {
             std::cerr << "Error listening on socket" << std::endl;
             close(sockfd);
@@ -85,6 +122,7 @@ void socket_serve() {
         int clientfd = accept(sockfd, NULL, NULL);
         if (clientfd < 0) {
             std::cerr << "Error accepting connection" << std::endl;
+            std::cerr << strerror(errno) << std::endl;
             close(sockfd);
             throw std::runtime_error("[hyprkool] Error accepting connection");
         }
@@ -93,42 +131,7 @@ void socket_serve() {
         close(clientfd);
     }
     close(sockfd);
-}
-
-void socket_init() {
-    const auto sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
-
-    sockaddr_un serverAddress = {0};
-    serverAddress.sun_family = AF_UNIX;
-
-    strncpy(serverAddress.sun_path, sock_path.c_str(), sizeof(serverAddress.sun_path) - 1);
-
-    if (connect(sockfd, (sockaddr*)&serverAddress, SUN_LEN(&serverAddress)) < 0) {
-        std::cout << "Couldn't connect to " << sock_path << "";
-        return;
-    }
-
-    char buffer[1024];
-    std::string partial_line;
-    while (true) {
-        ssize_t bytes_read = read(sockfd, buffer, sizeof(buffer));
-        if (bytes_read < 0) {
-            std::cerr << "Error reading from socket" << std::endl;
-            break;
-        } else if (bytes_read == 0) {
-            // End of stream (socket closed)
-            break;
-        }
-
-        std::istringstream iss(partial_line + std::string(buffer, bytes_read));
-        std::string line;
-        while (std::getline(iss, line)) {
-            std::cout << "Received line: " << line << std::endl;
-        }
-
-        partial_line = line;
-    }
-    close(sockfd);
+    auto _ = std::filesystem::remove(sock_path);
 }
 
 inline CFunctionHook* g_pWorkAnimHook = nullptr;
@@ -136,7 +139,34 @@ typedef void (*origStartAnim)(CWorkspace*, bool, bool, bool);
 void hk_workspace_anim(CWorkspace* thisptr, bool in, bool left, bool instant) {
     SAnimationPropertyConfig* conf = (thisptr->m_fAlpha.getConfig());
     std::string style = conf->pValues->internalStyle;
-    conf->pValues->internalStyle = "slidevert";
+
+    switch (anim_dir) {
+        case Animation::None: {
+            instant = true;
+        } break;
+        case Animation::Left: {
+            left = false;
+            conf->pValues->internalStyle = "slide";
+        } break;
+        case Animation::Right: {
+            left = true;
+            conf->pValues->internalStyle = "slide";
+        } break;
+        case Animation::Up: {
+            left = false;
+            conf->pValues->internalStyle = "slidevert";
+        } break;
+        case Animation::Down: {
+            left = true;
+            conf->pValues->internalStyle = "slidevert";
+        } break;
+        case Animation::Fade: {
+            conf->pValues->internalStyle = "fade";
+        } break;
+        default: {
+            instant = true;
+        } break;
+    }
 
     (*(origStartAnim)g_pWorkAnimHook->m_pOriginal)(thisptr, in, left, instant);
 
@@ -170,12 +200,12 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
     sock_path = get_socket_path();
 
     init_hooks();
-    std::thread sock_thread(socket_serve);
-    sock_thread.detach();
+    sock_thread = std::thread(socket_serve);
 
     return {"hyprkool", "hyprkool yea", "thrombe", "0.0.1"};
 }
 
 APICALL EXPORT void PLUGIN_EXIT() {
-    // ...
+    exit_flag = true;
+    sock_thread.join();
 }
