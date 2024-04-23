0242a0f18e60883113ecf1d97a6bf10cb8dc2bfc
thrombe <yash.n.thombare620@gmail.com>
2024-04-24 05:06:04 +0530

    typo in error

--- src/command.rs
+++ src/command.rs
@@ -444,7 +444,7 @@ impl Command {
                                 .await?;
                         } else {
                             return Err(anyhow!(
-                                "hyprkool plugin muust be running for this feature to work."
+                                "hyprkool plugin must be running for this feature to work."
                             ));
                         }
                     }
