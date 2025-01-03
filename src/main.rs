77f8910644949fe0e6bf017ed768c22026f626b1
thrombe <yash.n.thombare620@gmail.com>
2025-01-04 02:51:05 +0530

    add back some types from earlier impl

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,293 @@
+use std::collections::HashMap;
+use std::path::PathBuf;
+use std::sync::Arc;
 
+use anyhow::{anyhow, Context, Result};
+use clap::{arg, command, Parser, Subcommand};
+use hyprland::data::Monitor;
+use hyprland::data::Monitors;
+use hyprland::dispatch::WorkspaceIdentifierWithSpecial;
+use hyprland::{
+    data::{Client, Clients, CursorPosition, Workspace},
+    dispatch::{Dispatch, DispatchType, WindowIdentifier},
+    shared::{HyprData, HyprDataActive, HyprDataActiveOptional},
+};
+use serde::{Deserialize, Serialize};
+use tokio::sync::Mutex;
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(default, deny_unknown_fields)]
+pub struct DaemonConfig {
+    // TODO: maybe
+    // pub enable: bool,
+    /// how long to wait for ipc responses before executing the command in ms
+    // pub ipc_timeout: u64,
+    pub fallback_commands: bool,
+
+    /// remember what workspace was last focused on an activity
+    pub remember_activity_focus: bool,
+
+    pub mouse: MouseConfig,
+}
+impl Default for DaemonConfig {
+    fn default() -> Self {
+        Self {
+            remember_activity_focus: true,
+            fallback_commands: true,
+            mouse: Default::default(),
+        }
+    }
+}
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(default, deny_unknown_fields)]
+pub struct MouseConfig {
+    pub switch_workspace_on_edge: bool,
+    /// mouse polling rate in ms
+    pub polling_rate: u64,
+    /// number of pixels to consider as edge
+    pub edge_width: u64,
+    /// push cursor inside margin when it loops
+    pub edge_margin: u64,
+}
+impl Default for MouseConfig {
+    fn default() -> Self {
+        Self {
+            switch_workspace_on_edge: true,
+            polling_rate: 300,
+            edge_width: 0,
+            edge_margin: 2,
+        }
+    }
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub enum MultiMonitorStrategy {
+    // all monitors share a common hyprkool workspace (same x y) acitvity:(x y w)
+    SeparateWorkspaces,
+
+    // activity:(x y)
+    SharedWorkspacesSyncActivities, // m1:a1w1 m2:a2w2 -> m1:a2w1 m2:a2w2 when switching activities
+    SharedWorkspacesUnsyncActivities,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(default, deny_unknown_fields)]
+pub struct Config {
+    pub activities: Vec<String>,
+    /// number of workspaces in x and y dimensions
+    pub workspaces: (i32, i32),
+    pub multi_monitor_strategy: MultiMonitorStrategy,
+    pub named_focii: HashMap<String, String>,
+    pub daemon: DaemonConfig,
+}
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            activities: vec!["default".into()],
+            workspaces: (2, 2),
+            multi_monitor_strategy: MultiMonitorStrategy::SharedWorkspacesUnsyncActivities,
+            named_focii: Default::default(),
+            daemon: Default::default(),
+        }
+    }
+}
+
+#[derive(Subcommand, Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub enum InfoCommand {
+    WaybarActivityStatus,
+    WaybarActiveWindow,
+
+    Submap,
+    Activities,
+    Workspaces,
+    AllWorkspaces,
+    ActiveWindow {
+        /// try to find smallest icon bigger/equal to this size in px
+        /// default is 0
+        /// returns the biggest found size if none is bigger than/equal to the specified size
+        #[arg(long, short = 's', default_value_t = 0)]
+        try_min_size: u16,
+
+        /// default value is the current icon theme
+        /// will use fallback theme is this is not found
+        #[arg(long, short)]
+        theme: Option<String>,
+    },
+    ActiveWorkspaceWindows {
+        /// try to find smallest icon bigger/equal to this size in px
+        /// default is 0
+        /// returns the biggest found size if none is bigger than/equal to the specified size
+        #[arg(long, short = 's', default_value_t = 0)]
+        try_min_size: u16,
+
+        /// default value is the current icon theme
+        /// will use fallback theme is this is not found
+        #[arg(long, short)]
+        theme: Option<String>,
+    },
+}
+
+#[derive(Subcommand, Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub enum Command {
+    Daemon {
+        #[arg(long, short, default_value_t = false)]
+        move_to_hyprkool_activity: bool,
+    },
+    DaemonQuit,
+    Info {
+        #[command(subcommand)]
+        command: InfoCommand,
+
+        #[arg(long, short, default_value_t = false)]
+        monitor: bool,
+    },
+    FocusWindow {
+        #[arg(long, short)]
+        address: String,
+    },
+    MoveRight {
+        #[arg(long, short, default_value_t = false)]
+        cycle: bool,
+
+        /// move focused window and move to workspace
+        #[arg(long, short = 'w', default_value_t = false)]
+        move_window: bool,
+    },
+    MoveLeft {
+        #[arg(long, short, default_value_t = false)]
+        cycle: bool,
+
+        /// move focused window and move to workspace
+        #[arg(long, short = 'w', default_value_t = false)]
+        move_window: bool,
+    },
+    MoveUp {
+        #[arg(long, short, default_value_t = false)]
+        cycle: bool,
+
+        /// move focused window and move to workspace
+        #[arg(long, short = 'w', default_value_t = false)]
+        move_window: bool,
+    },
+    MoveDown {
+        #[arg(long, short, default_value_t = false)]
+        cycle: bool,
+
+        /// move focused window and move to workspace
+        #[arg(long, short = 'w', default_value_t = false)]
+        move_window: bool,
+    },
+    NextActivity {
+        #[arg(long, short, default_value_t = false)]
+        cycle: bool,
+
+        /// move focused window and move to workspace
+        #[arg(long, short = 'w', default_value_t = false)]
+        move_window: bool,
+    },
+    PrevActivity {
+        #[arg(long, short, default_value_t = false)]
+        cycle: bool,
+
+        /// move focused window and move to workspace
+        #[arg(long, short = 'w', default_value_t = false)]
+        move_window: bool,
+    },
+    SwitchToActivity {
+        /// <activity name>
+        #[arg(short, long)]
+        name: String,
+
+        /// move focused window and move to workspace
+        #[arg(long, short = 'w', default_value_t = false)]
+        move_window: bool,
+    },
+    SwitchToWorkspaceInActivity {
+        /// <workspace name>
+        #[arg(short, long)]
+        name: String,
+
+        /// move focused window and move to workspace
+        #[arg(long, short = 'w', default_value_t = false)]
+        move_window: bool,
+    },
+    SwitchToWorkspace {
+        /// <activity name>:<workspace name>
+        #[arg(short, long)]
+        name: String,
+
+        /// move focused window and move to workspace
+        #[arg(long, short = 'w', default_value_t = false)]
+        move_window: bool,
+    },
+    ToggleSpecialWorkspace {
+        #[arg(short, long)]
+        name: String,
+
+        /// move focused window and move to workspace
+        #[arg(long, short = 'w', default_value_t = false)]
+        move_window: bool,
+
+        #[arg(short, long, requires("move_window"))]
+        silent: bool,
+    },
+    SwitchNamedFocus {
+        /// set current named focus to none if name not provided
+        #[arg(short, long)]
+        name: String,
+
+        /// move focused window and move to workspace
+        #[arg(long, short = 'w', default_value_t = false)]
+        move_window: bool,
+    },
+    SetNamedFocus {
+        /// lock current named focus if none
+        #[arg(short, long)]
+        name: String,
+    },
+    ToggleOverview,
+}
+
+#[derive(Parser, Debug, Clone)]
+#[command(author, version, about)]
+struct Cli {
+    /// Specify a custom config directory
+    #[arg(short, long)]
+    pub config_dir: Option<String>,
+
+    #[command(subcommand)]
+    pub command: Command,
+
+    /// don't use daemon for this command even if one is active (mainly useful for debugging)
+    #[arg(long)]
+    pub force_no_daemon: bool,
+}
+
+impl Cli {
+    fn config(&self) -> Result<Config> {
+        let config = self
+            .config_dir
+            .clone()
+            .map(PathBuf::from)
+            .or(dirs::config_dir().map(|pb| pb.join("hypr")))
+            .map(|pb| pb.join("hyprkool.toml"))
+            .filter(|p| p.exists())
+            .map(std::fs::read_to_string)
+            .transpose()?
+            .map(|s| toml::from_str::<Config>(&s))
+            .transpose()?
+            .unwrap_or(Config::default());
+        match config.workspaces {
+            (0, _) | (_, 0) => {
+                return Err(anyhow!("Use non zero workspace grid dimentions in config"));
+            }
+            _ => (),
+        }
+        Ok(config)
+    }
+}
+#[tokio::main]
+async fn main() -> Result<()> {
+    Ok(())
+}
